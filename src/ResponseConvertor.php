653a6725d2c417d5eb100947d24e96586f7025bb
Arnold Daniels <arnold@jasny.net>
2017-01-23 12:35:30 +0100

    Added RequestConvertor and ResponseConvertor
    Improved testability and code quality
    Converting a browserkit request to a psr-7 request has been moved from the Connector to a seperate class
    Same for response

---
+++ src/ResponseConvertor.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Jasny\Codeception;
+
+use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\BrowserKit\Response as BrowserKitResponse;
+
+/**
+ * Convert a PSR-7 response to a codeception response
+ */
+class ResponseConvertor
+{
+    /**
+     * Convert a PSR-7 response to a codeception response
+     * 
+     * @param ResponseInterface $psrResponse
+     * @return BrowserKitResponse
+     */
+    public function convert(ResponseInterface $psrResponse)
+    {
+        return new BrowserKitResponse(
+            (string)$psrResponse->getBody(),
+            $psrResponse->getStatusCode() ?: 200,
+            $this->flattenHeaders($psrResponse->getHeaders())
+        );
+    }
+    
+    /**
+     * Flatten headers
+     * 
+     * @param array $headers
+     * @return array
+     */
+    protected function flattenHeaders(array $headers)
+    {
+        foreach ($headers as &$value) {
+            $value = is_array($value) ? end($value) : $value;
+        }
+        
+        return $headers;
+    }
+}
